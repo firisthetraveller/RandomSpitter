60ac7a9bf54b4b2292f405d529b59a43190a64be
gluong <16323642+guy-luong@users.noreply.github.com>
2023-02-14 12:01:00 +0100

    final commit

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,40 @@
-#[macro_use]
-extern crate json;
+use serde_derive::Deserialize;
+use serde_json;
+use std::env;
+use std::fs;
 
-use rand::Rng;
+#[derive(Debug, Deserialize, Clone)]
+struct Themes {
+    category: String,
+    title: String,
+    themes: Vec<String>,
+}
+
+fn usage() {
+    println!("Usage: cargo run -- <category>");
+    println!("category: [general | faces]");
+}
 
 fn main() {
-    let json_file_path = Path::new("assets/inktoberthemes.json");
-    let file = File::open(json_file_path);
+    let args: Vec<String> = env::args().collect();
+    let contents = fs::read_to_string("assets/inktoberthemes.json").expect("File doesn't exist.");
+    // let data = fs::read_to_string("./src/data.json").unwrap();
+    let parsed = serde_json::from_str::<Vec<Themes>>(&contents).expect("JSON was not well formed");
 
-    println!("Hello, world!");
+    if args.len() == 1 {
+        usage();
+        return;
+    }
+
+    // println!("{:?}", args);
+
+    let category = &args[1];
+    let themes: Vec<String> = parsed
+        .into_iter()
+        .filter(|theme| &theme.category == category)
+        .flat_map(|t| t.themes)
+        .collect::<Vec<String>>();
+
+    let r: usize = rand::random();
+    println!("{}", themes[r % themes.len()]);
 }
