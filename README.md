edb0f3d56585f719cba11d6a1927d2329121c46f
gluong <16323642+guy-luong@users.noreply.github.com>
2023-02-23 22:56:01 +0100

    Add: multiple tags

---
+++ README.md
@@ -0,0 +1,17 @@
+# RandomSpitter
+
+A word generator, created to get a random theme for drawing.
+
+## Installation
+
+We use `cargo`, the project manager from Rust.
+```sh
+$ > cargo build
+```
+
+## Usage
+You need to mention the type of the words you want to generate.
+It should be a category name in the `assets/inktoberthemes.json` file.
+```sh
+$ > cargo run -- <category>
+```
--- src/main.rs
+++ src/main.rs
@@ -4,36 +4,41 @@ use std::env;
 use std::fs;
 
 #[derive(Debug, Deserialize, Clone)]
-struct Themes {
+struct Theme {
     category: String,
     title: String,
     themes: Vec<String>,
 }
 
 fn usage() {
-    println!("Usage: cargo run -- <category>");
+    println!("Usage: cargo run -- [<category> ...]");
     println!("category: [general | faces]");
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let contents = fs::read_to_string("assets/inktoberthemes.json").expect("File doesn't exist.");
-    let parsed = serde_json::from_str::<Vec<Themes>>(&contents).expect("JSON was not well formed");
+    let parsed = serde_json::from_str::<Vec<Theme>>(&contents).expect("JSON was not well formed");
 
     if args.len() == 1 {
         usage();
         return;
     }
 
-    // println!("{:?}", args);
+    for (i, a) in args.iter().enumerate() {
+        println!("{}, {}", i, a);
+    }
 
-    let category = &args[1];
-    let themes: Vec<String> = parsed
-        .into_iter()
-        .filter(|theme| &theme.category == category)
-        .flat_map(|t| t.themes)
-        .collect::<Vec<String>>();
+    let categories = &args[1..];
+    for category in categories {
+        let themes = parsed
+            .to_vec() // copy
+            .into_iter()
+            .filter(|theme| &theme.category == category)
+            .flat_map(|t| t.themes)
+            .collect::<Vec<String>>();
 
-    let r: usize = rand::random();
-    println!("{}", themes[r % themes.len()]);
+        let r: usize = rand::random();
+        println!("{}", themes[r % themes.len()]);
+    }
 }
