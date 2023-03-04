19e89b54263da348b0d47997d3e57892cfa2ada2
gluong <16323642+guy-luong@users.noreply.github.com>
2023-03-04 10:37:41 +0100

    Add: unique & more themes

--- src/main.rs
+++ src/main.rs
@@ -25,20 +25,31 @@ fn main() {
         return;
     }
 
-    for (i, a) in args.iter().enumerate() {
-        println!("{}, {}", i, a);
-    }
+    // for (i, a) in args.iter().enumerate() {
+    //     println!("{}, {}", i, a);
+    // }
 
     let categories = &args[1..];
     for category in categories {
-        let themes = parsed
-            .to_vec() // copy
+        let mut themes = parsed
+            .to_vec()
             .into_iter()
             .filter(|theme| &theme.category == category)
             .flat_map(|t| t.themes)
             .collect::<Vec<String>>();
 
+        // remove duplicates
+        // sort + dedup
+        // source : https://www.reddit.com/r/rust/comments/38zzbk/best_way_to_remove_duplicates_from_a_veclist/
+        themes.sort();
+        themes.dedup_by(|a, b| a == b);
+
+        if themes.len() == 0 {
+            eprintln!("no themes linked to the category {}", category);
+            continue;
+        }
+
         let r: usize = rand::random();
-        println!("{}", themes[r % themes.len()]);
+        println!("{}: {}", category, themes[r % themes.len()]);
     }
 }
